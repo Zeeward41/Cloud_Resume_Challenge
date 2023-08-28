01e2b024629a488a5de47bab3da411226602fd20
Zeeward41 <zeeward41@gmail.com>
2023-08-28 02:59:50 +0200

    mise au propre de la fonction get_visiteurs (obtenir le nombre de visiteurs)

--- Front-end/script.js
+++ Front-end/script.js
@@ -1,5 +1,36 @@
 "use strict";
 
+//"
+const site_url = "https://zeeward41.com";
+const api_url = "https://mzzoptlt7f.execute-api.eu-west-3.amazonaws.com/dev";
+
+function get_visiteurs(url_api, site_api) {
+  fetch(url_api, {
+    method: "GET", // ou la méthode appropriée
+    // Autres options de requête si nécessaires
+    headers: {
+      "Access-Control-Allow-Origin": site_api,
+    },
+  })
+    .then((response) => {
+      // Log de la réponse HTTP complète
+      console.log("Réponse HTTP complète :", response);
+      return response.json();
+    })
+    .then((data) => {
+      // Log du contenu JSON de la réponse
+      console.log("Contenu JSON de la réponse :", data);
+
+      document.querySelector(".n_visitors").textContent = data["n_visiteurs"];
+    })
+    .catch((error) => {
+      // Log des erreurs s'il y en a
+      console.error("Erreur lors de la requête fetch :", error);
+    });
+}
+
+get_visiteurs(api_url, site_url);
+
 // // Vérifier l'état de la session ou du cookie
 // if (!sessionStorage.getItem("apiActivated")) {
 //   // Activer l'API via une requête AJAX ou Fetch
@@ -22,26 +53,3 @@
 // } else {
 //   console.log("L'API est déjà activée pour ce visiteur");
 // }
-
-fetch("https://mzzoptlt7f.execute-api.eu-west-3.amazonaws.com/dev", {
-  method: "GET", // ou la méthode appropriée
-  // Autres options de requête si nécessaires
-  headers: {
-    "Access-Control-Allow-Origin": "https://zeeward41.com",
-  },
-})
-  .then((response) => {
-    // Log de la réponse HTTP complète
-    console.log("Réponse HTTP complète :", response);
-    return response.json();
-  })
-  .then((data) => {
-    // Log du contenu JSON de la réponse
-    console.log("Contenu JSON de la réponse :", data);
-    document.querySelector(".n_visitors").textContent = data["n_visiteurs"];
-    console.log(`La nombre de visiteur est de ${data["n_visiteurs"]}`);
-  })
-  .catch((error) => {
-    // Log des erreurs s'il y en a
-    console.error("Erreur lors de la requête fetch :", error);
-  });
