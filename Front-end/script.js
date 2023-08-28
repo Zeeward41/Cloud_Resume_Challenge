046e3bf2b8dfd5a254924e1ef2afe019db6f0237
Zeeward41 <zeeward41@gmail.com>
2023-08-28 03:33:08 +0200

    finish fonction update_visiteurs + update uniquement une fois la sessionStorage vide

--- Front-end/script.js
+++ Front-end/script.js
@@ -29,27 +29,31 @@ function get_visiteurs(url_api, site_api) {
     });
 }
 
+function update_visiteurs(api_url, site_url) {}
+
 get_visiteurs(api_url, site_url);
 
-// // Vérifier l'état de la session ou du cookie
-// if (!sessionStorage.getItem("apiActivated")) {
-//   // Activer l'API via une requête AJAX ou Fetch
-//   fetch("https://mzzoptlt7f.execute-api.eu-west-3.amazonaws.com/dev/", {
-//     method: "GET", // ou la méthode appropriée
-//     // Autres options de requête si nécessaires
-//     headers: {
-//       "Access-Control-Allow-Origin": "https://zeeward41.com",
-//     },
-//   })
-//     .then((response) => response.json())
-//     .then((data) => {
-//       console.log("API activée avec succès", data);
-//       // Mettre à jour la session ou le cookie pour indiquer l'activation
-//       sessionStorage.setItem("apiActivated", "true");
-//     })
-//     .catch((error) => {
-//       console.error("Erreur lors de l'activation de l'API", error);
-//     });
-// } else {
-//   console.log("L'API est déjà activée pour ce visiteur");
-// }
+// Vérifier l'état de la session ou du cookie
+if (!sessionStorage.getItem("apiActivated")) {
+  // Activer l'API via une requête AJAX ou Fetch
+  fetch(api_url, {
+    method: "POST",
+    headers: {
+      "Access-Control-Allow-Origin": site_url,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("API activée avec succès", data);
+
+      document.querySelector(".n_visitors").textContent = data["n_visiteurs"];
+
+      // Mettre à jour la session ou le cookie pour indiquer l'activation
+      sessionStorage.setItem("apiActivated", "true");
+    })
+    .catch((error) => {
+      console.error("Erreur lors de l'activation de l'API", error);
+    });
+} else {
+  console.log("L'API est déjà activée pour ce visiteur");
+}
