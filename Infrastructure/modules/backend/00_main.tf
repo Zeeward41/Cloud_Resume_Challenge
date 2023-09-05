99bf42fad72faaf1020d629d61c9c4421d248f5c
Zeeward41 <zeeward41@gmail.com>
2023-09-05 03:27:26 +0200

    ajout Iam Role pour lambda --> DynamoDB (full access)

--- Infrastructure/modules/backend/00_main.tf
+++ Infrastructure/modules/backend/00_main.tf
@@ -8,3 +8,24 @@ provider "aws" {
 # Variables
 
 variable "region_base" {}
+
+
+# Creation role pour lambda --> DynamoDB
+
+resource "aws_iam_role" "lambda_DynamoDB_Faccess" {
+    name = "lambda_DynamoDB_Faccess"
+    description = "Iam Role pour qu'une lambda est un acces total à DynamoDB"
+    managed_policy_arns = ["arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"]
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17",
+        Statement = [
+            {
+                Action = "sts:AssumeRole",
+                Effect = "Allow",
+                Principal = {
+                    Service = "lambda.amazonaws.com"
+                }
+            }
+        ]
+    })
+}
