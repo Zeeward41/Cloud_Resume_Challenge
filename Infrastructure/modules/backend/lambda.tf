9fea4aacd5d960317f6ff69fb6a1fe818d772c25
Zeeward41 <zeeward41@gmail.com>
2023-09-19 02:13:57 +0200

    ajout permission pour lambda
    ajouts de aws_lambda_permission pour les 2 lambdas (allow API gateway d'utiliser les 2 lambdas)

--- Infrastructure/modules/backend/lambda.tf
+++ Infrastructure/modules/backend/lambda.tf
@@ -20,3 +20,24 @@ resource "aws_lambda_function" "lambda_modify_nVisiteurs" {
   filename      = "modules/backend/lambda_zeeward41_modify.zip"
   handler       = "modify_nombreVisiteurs.lambda_handler"
 }
+
+# Creation permission accès lambda (GET)
+
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda_get_visiteurs.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn = "arn:aws:execute-api:${var.region_base}:585355206799:${aws_api_gateway_rest_api.zeeward41_api_gateway.id}/*/GET${aws_api_gateway_resource.zeeward41_get_visiteurs.path}"
+}
+
+# Creation permission accès lambda (POST)
+
+resource "aws_lambda_permission" "apigw_lambda_post" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda_modify_nVisiteurs.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn = "arn:aws:execute-api:${var.region_base}:585355206799:${aws_api_gateway_rest_api.zeeward41_api_gateway.id}/*/POST${aws_api_gateway_resource.zeeward41_get_visiteurs.path}"
+ 
+}
