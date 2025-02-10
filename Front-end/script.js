40b8cc60d332293833c763e4197c9ee49047bd9b
Zeeward41 <zeeward41@gmail.com>
2025-02-10 21:43:42 +0100

    fix: api_url

--- Front-end/script.js
+++ Front-end/script.js
@@ -2,7 +2,7 @@
 
 //"
 const site_url = "https://zeeward41.com";
-const api_url = "https://15peguj556.execute-api.eu-west-3.amazonaws.com/dev/lambda-to-dynamo-zeeward41";
+const api_url = "https://yyubti05ob.execute-api.eu-west-3.amazonaws.com/dev/lambda-to-dynamo-zeeward41";
 
 function get_visiteurs(api_url, site_url) {
   fetch(api_url, {
